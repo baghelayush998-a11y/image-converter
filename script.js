6ac5644e2d405ddac760f235ad058f57a82f3adf
baghelayush998-a11y <baghelayush998@gmail.com>
2026-05-12 19:52:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const fileInput = document.getElementById("fileInput");
+const preview = document.getElementById("preview");
+const downloadBtn = document.getElementById("downloadBtn");
+
+let uploadedImage = null;
+
+fileInput.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if(file){
+        const reader = new FileReader();
+
+        reader.onload = function(e){
+            uploadedImage = e.target.result;
+            preview.src = uploadedImage;
+        }
+
+        reader.readAsDataURL(file);
+    }
+});
+
+function convertImage(){
+    const format = document.getElementById("format").value;
+
+    if(!uploadedImage){
+        alert("Please upload image first");
+        return;
+    }
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+
+    img.onload = function(){
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        ctx.drawImage(img,0,0);
+
+        const converted = canvas.toDataURL(`image/${format}`);
+
+        preview.src = converted;
+        downloadBtn.href = converted;
+        downloadBtn.download = `converted-image.${format}`;
+    }
+
+    img.src = uploadedImage;
+}
